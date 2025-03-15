5f272a01bbef31c336754e24ef8f5226b61a2d87
shivam108ku <shivam108ku@gmail.com>
2025-03-15 19:00:54 +0530

    planet complete

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import "./style.css";
 import * as THREE from 'three'; 
 import gsap from "gsap";
+import { RGBELoader } from "three/examples/jsm/Addons.js";
+
+ 
 
 // Setup scene, camera and renderer
 const scene = new THREE.Scene();
@@ -17,14 +20,52 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 const  orbitradius = 4.5;
 const radius = 1.5;
 const segments = 64;
-const colors = [0x00ff00, 0x0000ff, 0xff00ff, 0xffff00];
-
+// const colors = [0x00ff00, 0x0000ff, 0xff00ff, 0xffff00];
+const textures = ['./csilla/color.png', './earth/map.jpg', './venus/map.jpg','./volcanic/color.png']
 const spheres = new THREE.Group();
 
+const starTexture = new THREE.TextureLoader().load('./stars.jpg');
+const starGeometry = new THREE.SphereGeometry(50, 64 ,64);
+const starMaterial = new THREE.MeshStandardMaterial({
+  map: starTexture,
+  side: THREE.BackSide
+})
+const starSphere = new THREE.Mesh(starGeometry , starMaterial);
+scene.add(starSphere);
+
+
+// Add ambient and directional lighting
+
+const loader = new RGBELoader();
+loader.load('https://dl.polyhaven.org/file/ph-assets/HDRIs/hdr/1k/flamingo_pan_1k.hdr', function (texture){
+texture.mapping = THREE.EquirectangularReflectionMapping;
+ 
+ 
+ 
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+scene.add(ambientLight);
+})
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+directionalLight.position.set(5, 5, 5);
+scene.add(directionalLight);
+
+// Enable physically correct lighting
+renderer.physicallyCorrectLights = true;
+renderer.toneMapping = THREE.ACESFilmicToneMapping;
+renderer.toneMappingExposure = 1;
+
+
 for(let i =0; i<4; i++){
+
+  const textureLoader = new THREE.TextureLoader();
+  const texture = textureLoader.load(textures[i])
+  texture.colorSpace = THREE.SRGBColorSpace;
+
   const geometry = new THREE.SphereGeometry(radius, segments, segments);
-  const material = new THREE.MeshBasicMaterial({ color: colors[i] });
+  const material = new THREE.MeshStandardMaterial({map : texture });
   const sphere = new THREE.Mesh(geometry, material);
+ 
 
   const angle = (i / 4) * (Math.PI * 2);
   sphere.position.x = orbitradius * Math.cos(angle);
